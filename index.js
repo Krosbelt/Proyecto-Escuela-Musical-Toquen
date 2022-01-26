2af33d21594111285e69ff6becb55d500e0ba083
Karen <karenvane129@gmail.com>
2022-01-26 14:23:56 -0500

    Landing page completa con funcionalidades

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('.menu-nav li');
+const navLi = document.querySelectorAll('nav .menu-nav .menu li');
 
 window.addEventListener('scroll', () => {
     let current = '';
+  
     sections.forEach( section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
@@ -10,10 +11,12 @@ window.addEventListener('scroll', () => {
             current = section.getAttribute('id');
         }
     })
+    
     navLi.forEach( li => {
         li.classList.remove("active");
         if(li.classList. contains(current)) {
             li.classList.add('active')
         }
-    })
-})
+    }) 
+}) 
+
