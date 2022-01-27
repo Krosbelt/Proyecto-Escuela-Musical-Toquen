7e017707bc683c1247b6ae776ee9f71f72717670
Jymmy Murillo <murillojymmy@gmail.com>
2022-01-26 23:13:05 -0500

    Organizando secciones

--- Piano/piano.js
+++ Piano/piano.js
@@ -1,4 +1,10 @@
 //Declarar constantes
+
+const divMenu = document.getElementById("divMenu");
+const menuBtn = document.getElementById("menuBtn");
+const imgBateria = document.getElementById("bateria");
+const imgMarimba = document.getElementById("marimba");
+
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
@@ -6,6 +12,40 @@ const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
 
+
+///////////////////// Open Menu ///////////////////////////
+
+function functionMenuBtn() {
+  if (divMenu.style.display === "" || divMenu.style.display === "none") {
+    divMenu.style.display = "flex";
+    menuBtn.style.color = "#D5E763";
+  } else {
+    divMenu.style.display = "none";
+    menuBtn.style.color = "#fff";
+  }
+}
+
+/////////////////////// Hover Menu ///////////////////////////////
+
+
+function imgDrumsHover() {
+  imgBateria.src = "../Image/bateria-hover.svg";
+}
+
+function imgDrumsDefault() {
+  imgBateria.src = "../Image/bateria-blanco-negro.png";
+}
+
+
+function imgMarimbaHover() {
+  imgMarimba.src = "../Image/marimba-hover.png";
+}
+
+function imgMarimbaDefault() {
+  imgMarimba.src = "../Image/marimba-negro-blanco.png";
+}
+
+///////////////////////////
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key) )
 })
@@ -29,4 +69,5 @@ document.addEventListener('keydown', e => {
   
     if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
     if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-  })
+})
+  
